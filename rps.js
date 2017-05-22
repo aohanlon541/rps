05e2f167250efaed3544cb3090eb1b0a227474e3
Allison O'Hanlon <allison.t.ohanlon@gmail.com>
2017-05-21 20:35:15 -0500

    html/css

---
+++ rps.js
@@ -0,0 +1,27 @@
+  // Initialize Firebase
+  // var config = {
+  //   apiKey: "AIzaSyAnuJ1uzW7hileZKIEgH5wtFSns8RNBai8",
+  //   authDomain: "rock-paper-scissors-f7d87.firebaseapp.com",
+  //   databaseURL: "https://rock-paper-scissors-f7d87.firebaseio.com",
+  //   projectId: "rock-paper-scissors-f7d87",
+  //   storageBucket: "rock-paper-scissors-f7d87.appspot.com",
+  //   messagingSenderId: "443522779877"
+  // };
+  // firebase.initializeApp(config);
+
+  // var database = firebase.database();
+
+  // var wins1 = 0;
+  // var losses1 = 0;
+  // var ties1 = 0;
+
+  // var wins2 = 0;
+  // var losses2 = 0;
+  // var ties2 = 0;
+  // var player2;
+  // var player1;
+  // var player1name;
+  // var player2name;
+
+var rock1 = $("#rock1").val();
+console.log(rock1);

82eb31c709e90c61cf2ba6dfc4ffa995c9f693b7
aohanlon541 <allison.t.ohanlon@gmail.com>
2017-05-22 21:22:40 -0500

    monday work

--- rps.js
+++ rps.js
@@ -1,27 +1,91 @@
-  // Initialize Firebase
-  // var config = {
-  //   apiKey: "AIzaSyAnuJ1uzW7hileZKIEgH5wtFSns8RNBai8",
-  //   authDomain: "rock-paper-scissors-f7d87.firebaseapp.com",
-  //   databaseURL: "https://rock-paper-scissors-f7d87.firebaseio.com",
-  //   projectId: "rock-paper-scissors-f7d87",
-  //   storageBucket: "rock-paper-scissors-f7d87.appspot.com",
-  //   messagingSenderId: "443522779877"
-  // };
-  // firebase.initializeApp(config);
+//database setup  
+  var config = {
+    apiKey: "AIzaSyCWSR8VIzdr2vr0ZxP6w7yIfBXLKAfZy7U",
+    authDomain: "rps-multi-e340c.firebaseapp.com",
+    databaseURL: "https://rps-multi-e340c.firebaseio.com",
+    projectId: "rps-multi-e340c",
+    storageBucket: "rps-multi-e340c.appspot.com",
+    messagingSenderId: "895340324044"
+  };
+  firebase.initializeApp(config);
 
-  // var database = firebase.database();
+  var database = firebase.database();
 
-  // var wins1 = 0;
-  // var losses1 = 0;
-  // var ties1 = 0;
+//on every click, sets up a player with an unique #
+  var playerNum = 0 
 
-  // var wins2 = 0;
-  // var losses2 = 0;
-  // var ties2 = 0;
-  // var player2;
-  // var player1;
-  // var player1name;
-  // var player2name;
+  $("#submitBtn").on("click", function() {
+    event.preventDefault();
+    playerNum++;
+    var submittedName = $("#name-input").val().trim();
+    console.log(submittedName); 
 
-var rock1 = $("#rock1").val();
-console.log(rock1);
+      database.ref().push({
+        name: submittedName,
+        playerNum: playerNum
+        });
+    return playerNum;
+  });
+
+  $(".imgBtn1").on("click", function () {
+    var player1choice = $(this).val();
+    console.log(player1choice)
+
+    $(".imgBtn2").on("click", function () {
+    var player2choice = $(this).val(); 
+    console.log(player2choice); 
+
+    function erase() {
+      $("#results").empty();
+    }
+
+   if (player1choice === player2choice) {
+      $("#results").append("Tie!");
+      setTimeout(erase, 5000);
+    }
+
+    if (player1choice === "rock" && player2choice === "paper") {
+     $("#results").append(player2choice + " wins!");
+      setTimeout(erase, 5000);
+    }
+
+    if (player1choice === "rock" && player2choice === "scissors") {
+     $("#results").append(player1choice + " wins!");
+      setTimeout(erase, 5000);
+    }
+
+    if (player1choice === "paper" && player2choice === "rock") {
+     $("#results").append(player1choice + " wins!");
+      setTimeout(erase, 5000);
+    }
+
+    if (player1choice === "paper" && player2choice === "scissors") {
+     $("#results").append(player2choice + " wins!");
+      setTimeout(erase, 5000);
+    }
+
+    if (player1choice === "scissors" && player2choice === "rock") {
+     $("#results").append(player2choice + " wins!");
+      setTimeout(erase, 5000);
+    }
+
+    if (player1choice === "scissors" && player2choice === "paper") {
+     $("#results").append(player1choice + " wins!");
+      setTimeout(erase, 5000);
+    }
+
+
+    }); 
+  });
+
+
+
+
+  var wins1 = 0;
+  var losses1 = 0;
+  var ties1 = 0;
+
+  var wins2 = 0;
+  var losses2 = 0;
+  var ties2 = 0;
+
